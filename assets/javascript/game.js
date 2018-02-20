f4a892d0c2a54ebed3bb729d256a6f4753b0480f
Elizabeth Lark <elizabethlark@Elizabeths-MBP.fios-router.home>
2018-02-19 19:06:31 -0500

    added gem images; updated code to reflect num of wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,32 +5,66 @@ var buttonNum3 = Math.floor(Math.random() * (12 - 1) + 1);
 var buttonNum4 = Math.floor(Math.random() * (12 - 1) + 1);
 var userEntryArray = [];
 var arrayTotal;
+var wins = 0;
+var losses = 0;
 
-/**** Random number between 19 - 120 is generated and displayed after html has loaded ****/
+
+/* Random number between 19 - 120 is generated and displayed after html 
+has loaded and game board is initialized */
 
 $( document ).ready(function() {
     $("#random-num").text(randomStartNum); 
+    $("#total-score").text("0");
+    $("#wins").text("Wins: " + wins)
+    $("#losses").text("Losses: " + losses)
     console.log("Random #: " + randomStartNum);
 });
 
-/**** Capture values that are randomly generated upon click of button ****/
+
+
+/* Capture values that are randomly generated upon click of button 
+& update total score*/
 
 $("#button1").on("click", function () {
     $("#button1").text(buttonNum1);
     userEntryArray.push(buttonNum1);
     console.log("Button #1: " + buttonNum1);
     totalScore();
-    $("#total-score").html(arrayTotal);
-    console.log ("total score: " + arrayTotal);
+    console.log ("total score: " + arrayTotal);   
+    if (arrayTotal === randomStartNum) {
+        $("#total-score").text(arrayTotal);
+        wins += 1;
+        $("#wins").text("Wins: " + wins);
+        
+        alert ("You win!");
+        // newGame();
+    }
+   
+    if (arrayTotal > randomStartNum) {
+        $("#total-score").text(arrayTotal);
+        losses += 1;
+        $("#losses").text("Losses: " + losses);
+        
+        alert ("You lose!");
+        // newGame();
+    }
     
 });
- 
 $("#button2").on("click", function () {
     $("#button2").text(buttonNum2);
     userEntryArray.push(buttonNum2);
     console.log("Button #2: " + buttonNum2);
     totalScore();
-    $("#total-score").html(arrayTotal);
+        if (arrayTotal === randomStartNum) {
+            wins += 1;
+            $("#wins").text("Wins: " + wins);
+            //newGame();
+        }
+        if (arrayTotal > randomStartNum) {
+            losses += 1;
+            $("#losses").text("Losses: " + losses);
+            //newGame();
+        }
     console.log ("total score: " + arrayTotal);
 });
 
@@ -39,7 +73,16 @@ $("#button3").on("click", function () {
     userEntryArray.push(buttonNum3);
     console.log("Button #3: " + buttonNum3);
     totalScore();
-    $("#total-score").html(arrayTotal);
+    if (arrayTotal === randomStartNum) {
+        wins += 1;
+        $("#wins").text("Wins: " + wins);
+        //newGame();
+    }
+    if (arrayTotal > randomStartNum) {
+        losses += 1;
+        $("#losses").text("Losses: " + losses);
+        //newGame();
+    }
     console.log ("total score: " + arrayTotal);
 });
 
@@ -48,12 +91,20 @@ $("#button4").on("click", function () {
     userEntryArray.push(buttonNum4);
     console.log("Button #4: " + buttonNum4);
     totalScore();
-    $("#total-score").html(arrayTotal);
+        if (arrayTotal === randomStartNum) {
+            wins += 1;
+            $("#wins").text("Wins: " + wins);
+            //newGame();
+        }
+        // alert("Nice work - you win! Can you do it again?")
+        if (arrayTotal > randomStartNum) {
+            losses += 1;
+            $("#losses").text("Losses: " + losses);
+            //newGame();
+        }
+        // alert("You Lose. Try again!")
     console.log ("total score: " + arrayTotal);
 });
-
-
-/**** Calculations ****/
 
 //function used to add the values in an array; passing it as a callback function in the total score function
 function addArrayValues (total, num) {
@@ -64,6 +115,18 @@ function totalScore () {
     arrayTotal = userEntryArray.reduce(addArrayValues);//using reduce method to reduce array into a single value by calling a function that adds all the values in the array
     $("#total-score").text(arrayTotal);
 }
+// Generate new random number for new game 
+function newGame () {
+    var newNum = Math.floor(Math.random() * (120 - 19) + 19);
+    $("#random-num").text(newNum);    
+    // userEntryArray = [];
+}
+
+//Things to fix:
+// 1. need to display array total after users last guess prior to starting new game
+// Note: right now; the total function runs after a click; need to get it outside of click
+// 2. figure out where to put new game function (function works just need proper placement)
+
 
 
 
